3202b5b45e87ab9fafce4a8157dd2e0fb8232ce4
tobiasvalbjoern <tvalbjoern@gmail.com>
2018-11-28 14:38:57 +0100

    removed binding

--- app/src/main/java/dk/e5pro5/foregroundservice/MainActivity.kt
+++ app/src/main/java/dk/e5pro5/foregroundservice/MainActivity.kt
@@ -4,36 +4,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.app.ActivityManager
-import android.content.ComponentName
 import android.content.Context
 import android.widget.Toast
 import android.content.Intent
-import android.content.ServiceConnection
-import android.os.AsyncTask
-import android.os.IBinder
-import android.util.Log
+
 
 class MainActivity : AppCompatActivity(){
-
-    var myService: MyService? = null
-    var isBound = false
-
-    private val myConnection = object : ServiceConnection {
-        override fun onServiceConnected(className: ComponentName,
-                                        service: IBinder) {
-            val binder = service as MyService.MyLocalBinder
-            myService = binder.getService()
-            isBound = true
-            getBackgroundNotification(applicationContext, myService).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
-        }
-        override fun onServiceDisconnected(name: ComponentName) {
-            isBound = false
-        }
-    }
-    fun showSteps() {
-        val currentSteps=myService?.getSteps()
-        toast(currentSteps.toString())
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,20 +20,18 @@ class MainActivity : AppCompatActivity(){
         // If the service is not running then start it
         if (!isServiceRunning(serviceClass)) {
             // Start the service
-
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
                 // Do something for OREO and above versions
+                //Similar to startService(Intent), but with an implicit promise that the
+                // Service will call startForeground(int, android.app.Notification) once it begins running.
                 startForegroundService(serviceIntent)
+
             } else{
                 // do something for phones running an SDK before OREO
                 startService(serviceIntent)
             }
-
-
-            bindService(serviceIntent, myConnection, Context.BIND_AUTO_CREATE)
         } else {
             toast("Service already running.")
-            bindService(serviceIntent, myConnection, Context.BIND_AUTO_CREATE)
         }
 
         button_start.setOnClickListener{
@@ -70,21 +44,13 @@ class MainActivity : AppCompatActivity(){
                     // do something for phones running an SDK before OREO
                     startService(serviceIntent)
                 }
-                bindService(serviceIntent, myConnection, Context.BIND_AUTO_CREATE)
             } else {
                 toast("Service already running.")
-                bindService(serviceIntent, myConnection, Context.BIND_AUTO_CREATE)
             }
         }
 
         // Button to stop the service
         button_stop.setOnClickListener{
-            //Unbind Service
-            try {
-                unbindService(myConnection)
-            } catch (e: IllegalArgumentException) {
-                Log.w("MainActivity", "Error Unbinding Service.")
-            }
             // If the service is running then stop it
             if (isServiceRunning(serviceClass)) {
                 // Stop the service
@@ -101,13 +67,7 @@ class MainActivity : AppCompatActivity(){
             } else {
                 toast("Service is stopped.")
             }
-            showSteps()
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        // Always call the superclass so it can save the view hierarchy state
-        super.onSaveInstanceState(outState)
     }
 
     // Custom method to determine whether a service is running
@@ -124,8 +84,8 @@ class MainActivity : AppCompatActivity(){
         return false
     }
 
-// Extension function to show toast message
-fun Context.toast(message:String){
+    // Extension function to show toast message
+    private fun Context.toast(message:String){
     Toast.makeText(applicationContext,message,Toast.LENGTH_SHORT).show()
 }}
 
